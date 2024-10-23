fc8838d0d72e8c3215d43a86c283756877f9c86a
Hilmyfrzl <hilmyfahrizal5@gmail.com>
2024-10-23 19:23:17 +0700

    update 1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home', function () {
+    return view('front.page');
+});
+
+Route::get('/page', function () {
+    return view('admin.admin');
+});
